3201fabc64324850151976238b088de167ae3381
troy <oconnort@go.stockton.edu>
2024-07-18 16:35:07 -0400

    SelfPortrait

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,13 +8,16 @@ return (
 <p className="aboutText">
     Hello, my name is Troy O'Connor and I am a junior at Stockton University
     studying computer science. I plan to graduate in the spring of 2025. I have a good grasp of
-    different kinds of technology and I have always enjoyed solving tech related problems.
+    different kinds of technology and I have always enjoyed solving tech related problems. 
+</p>
+ <img src="./images/selfPortrait.jpeg" alt="" style={{width: "35vw"}}/>
+<p className="aboutText">
     I am skilled in web building with React.js, JavaScript, CSS, and HTML. I also have a 
     good foundation with java, and Github. I am intruiged by front-end web design and I often find myself
     doing it for personal enjoyment. Finding a career in the front-end field is a dream of mine.
 </p>
+
 </div>
 )
 }
-
 export default About;
